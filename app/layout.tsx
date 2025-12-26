99f68712a620eb9b04619ade83c14aff64af31e5
alihamza3389 <hamzairshad@gmail.com>
2025-12-27 00:42:20 +0500

    wrap app with LangProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,9 @@ import "./globals.css";
 import Nav from "./components/Nav";
 import { LangProvider } from "./lang-provider";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="zh" suppressHydrationWarning>
+    <html lang="en">
       <body className="bg-neutral-900 text-white">
         <LangProvider>
           {children}
